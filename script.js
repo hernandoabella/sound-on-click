7bf5152a614dd559c14e835b3ea012d9e51d962a
Hernando Abella <hernandoabella@gmail.com>
2023-02-14 16:08:14 -0500

    Código script.js redactorizado y mejorado

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-function ding() {
-    var sound = new  Audio("https://www.soundjay.com/misc/sounds/small-bell-ring-01a.mp3");  
-    sound.play();
-}
-
-
-document.getElementsByTagName("button")[0].addEventListener("click", ding);
+document.addEventListener("DOMContentLoaded", () => {
+  const ding = () => new Audio("https://www.soundjay.com/misc/sounds/small-bell-ring-01a.mp3").play();
+  document.getElementById("my-button").addEventListener("click", ding);
+});
