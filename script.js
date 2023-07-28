5251a63fde238579df25bfc54f58704f65ec1443
Hernando Abella <hernandoabella@gmail.com>
2023-07-28 16:50:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const ding = () => new Audio("https://www.soundjay.com/misc/sounds/small-bell-ring-01a.mp3").play();
-  document.getElementById("my-button").addEventListener("click", ding);
+  const button = document.getElementById("my-button");
+  const ding = () => {
+    new Audio("sounds/small-bell-ring-01a.mp3").play();
+    // Agrega la clase 'clicked' al botón para activar la animación
+    button.classList.add("clicked");
+    // Quita la clase 'clicked' después de un breve tiempo (500 ms) para que la animación se repita en el siguiente clic.
+    setTimeout(() => {
+      button.classList.remove("clicked");
+    }, 500);
+  };
+  button.addEventListener("click", ding);
 });
